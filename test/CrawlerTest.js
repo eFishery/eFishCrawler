64862de1c5c6f7c6447e9f472b1f77d1ed07183b
ans4175 <ans4175@gmail.com>
2016-06-12 11:49:58 +0700

    get access token

--- test/CrawlerTest.js
+++ test/CrawlerTest.js
@@ -8,10 +8,22 @@ describe('Crawler eFish', function() {
 	var crawler;
 	var consumer_key = process.env.CONSUMER_KEY;
 	var consumer_secret = process.env.CONSUMER_SECRET;
+	var consumer_identifier = process.env.CONSUMER_IDENTIFIER;
+	var consumer_password = process.env.CONSUMER_PASSWORD;
 	it('should be generate engine', function(){
 		crawler = new Crawler(consumer_key, consumer_secret);
 		expect(crawler).to.be.an('object');
 		expect(crawler.getAccessToken).to.be.an('function');
 	});
-	it('should be return access token');
+	it('should be return access token', function(done){
+		crawler.getAccessToken(consumer_identifier,consumer_password)
+			.then(function(oauth){
+				expect(oauth).to.be.an('object');
+				expect(oauth).to.have.ownProperty('token');
+				expect(oauth).to.have.ownProperty('secret');
+				expect(crawler.oauth_access_token).to.be.an('string');
+				expect(crawler.oauth_access_token_secret).to.be.an('string');
+				done();
+			});
+	});
 })
