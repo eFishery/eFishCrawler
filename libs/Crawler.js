7bc538b1edb5d851d9014a4b1bdf303224540290
ans4175 <ans4175@gmail.com>
2016-06-21 14:40:26 +0700

    get all api contents

--- libs/Crawler.js
+++ libs/Crawler.js
@@ -1,10 +1,19 @@
 var OAuth = require('oauth');
 var fs = require('fs');
 var Promise = require("bluebird");
+var qs = require('querystring');
 
 var url_request_token = 'https://oauth.8villages.com/tokens/request-token';
 var url_access_token = 'https://accounts.8villages.com/authentication';
 var url_auth = 'https://accounts.8villages.com/authentication';
+var url_resource = 'https://conversation.8villages.com/1.0/contents/';
+
+var extend = function (obj1,obj2) {
+    var obj3 = {};
+    for (var attrname in obj1) { obj3[attrname] = obj1[attrname]; }
+    for (var attrname in obj2) { obj3[attrname] = obj2[attrname]; }
+    return obj3;
+}
 
 var Engine = function(consumerKey,consumerSecret){
 	this.oauth =new OAuth.OAuth(
@@ -20,7 +29,7 @@ var Engine = function(consumerKey,consumerSecret){
 	this.oauth_access_token_secret = null;
 };
 
-Engine.prototype.getAccessToken = function(identifier,password,callback){
+Engine.prototype.getAccessToken = function(identifier, password, callback){
 	var that = this;
 	return new Promise(function(resolve, reject){
 		that.oauth.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results){
@@ -46,6 +55,46 @@ Engine.prototype.getAccessToken = function(identifier,password,callback){
 			}
 		});
 	});
+};
+
+Engine.prototype.getContent = function(objectType, paramFilter, paramSort, callback){
+	var that = this;
+	return new Promise(function(resolve, reject){
+		var url_to_get = (objectType) ? url_resource + objectType : url_resource;
+		url_to_get += '?';
+		var paramAll = extend(paramFilter,paramSort);
+		if (paramAll) url_to_get += qs.stringify(paramAll);
+		that.oauth.getProtectedResource(url_to_get, "GET", that.oauth_access_token, that.oauth_access_token_secret,  function (error, data, response) {
+			if(error) {
+				reject(error);
+		  		if (typeof callback === 'function') callback(error);
+			} else { 
+				var parsedData = JSON.parse(data);
+				resolve(parsedData)
+				if (typeof callback === 'function') callback(null, parsedData);
+			}
+		});
+	});
+};
+
+Engine.prototype.getArticles = function(paramFilter, paramSort, callback){
+	var that = this;
+	return that.getContent('articles', paramFilter, paramSort, callback);
+};
+
+Engine.prototype.getQuestions = function(paramFilter, paramSort, callback){
+	var that = this;
+	return that.getContent('questions', paramFilter, paramSort, callback);
+};
+
+Engine.prototype.getTips = function(paramFilter, paramSort, callback){
+	var that = this;
+	return that.getContent('tips', paramFilter, paramSort, callback);
+};
+
+Engine.prototype.getForums = function(paramFilter, paramSort, callback){
+	var that = this;
+	return that.getContent('forums', paramFilter, paramSort, callback);
 };
 
 module.exports = Engine;
--- test/CrawlerTest.js
+++ test/CrawlerTest.js
@@ -24,11 +24,218 @@ describe('Crawler eFish', function() {
 				expect(crawler.oauth_access_token).to.be.an('string');
 				expect(crawler.oauth_access_token_secret).to.be.an('string');
 				done();
+			})
+			.catch(function(err){
+				console.error(err);
+				done();
 			});
 	});
-	it('should be get contents');
-	it('should be get specific contents');
-	it('should be get specific contents with params filter');
-	it('should be get specific contents with params sorting');
-	it('should be get articles');
+	it('should be get contents', function(done){
+		crawler.getContent()
+			.then(function(contents){
+				expect(contents).to.be.an('object');
+				expect(contents).to.have.ownProperty('totalRecords');
+				expect(contents.totalRecords).to.be.an('number');
+				expect(contents).to.have.ownProperty('totalDisplay');
+				expect(contents.totalDisplay).to.be.an('number');
+				expect(contents).to.have.ownProperty('data');
+				expect(contents).to.have.ownProperty('hasNext');
+				//data
+				expect(contents.data[0]).to.have.ownProperty('type');
+				expect(contents.data[0]).to.have.ownProperty('conversationId');
+				expect(contents.data[0]).to.have.ownProperty('state');
+				expect(contents.data[0]).to.have.ownProperty('privacy');
+				expect(contents.data[0]).to.have.ownProperty('privacy');
+				expect(contents.data[0]).to.have.ownProperty('title');
+				expect(contents.data[0]).to.have.ownProperty('content');
+				expect(contents.data[0]).to.have.ownProperty('summary');
+				expect(contents.data[0]).to.have.ownProperty('attachment');
+				expect(contents.data[0]).to.have.ownProperty('tags');
+				done();
+			})
+			.catch(function(err){
+				console.error(err);
+				done();
+			});
+	});
+	it('should be get specific contents articles', function(done){
+		crawler.getArticles()
+			.then(function(contents){
+				expect(contents).to.be.an('object');
+				expect(contents).to.have.ownProperty('totalRecords');
+				expect(contents.totalRecords).to.be.an('number');
+				expect(contents).to.have.ownProperty('totalDisplay');
+				expect(contents.totalDisplay).to.be.an('number');
+				expect(contents).to.have.ownProperty('data');
+				expect(contents).to.have.ownProperty('hasNext');
+				//data
+				expect(contents.data[0]).to.have.ownProperty('type');
+				expect(contents.data[0]).to.have.ownProperty('conversationId');
+				expect(contents.data[0]).to.have.ownProperty('state');
+				expect(contents.data[0]).to.have.ownProperty('privacy');
+				expect(contents.data[0]).to.have.ownProperty('title');
+				expect(contents.data[0]).to.have.ownProperty('content');
+				expect(contents.data[0]).to.have.ownProperty('summary');
+				expect(contents.data[0]).to.have.ownProperty('attachment');
+				expect(contents.data[0]).to.have.ownProperty('tags');
+				done();
+			})
+			.catch(function(err){
+				console.error(err);
+				done();
+			});
+	});
+	it('should be get specific contents questions', function(done){
+		crawler.getQuestions()
+			.then(function(contents){
+				expect(contents).to.be.an('object');
+				expect(contents).to.have.ownProperty('totalRecords');
+				expect(contents.totalRecords).to.be.an('number');
+				expect(contents).to.have.ownProperty('totalDisplay');
+				expect(contents.totalDisplay).to.be.an('number');
+				expect(contents).to.have.ownProperty('data');
+				expect(contents).to.have.ownProperty('hasNext');
+				//data
+				expect(contents.data[0]).to.have.ownProperty('type');
+				expect(contents.data[0]).to.have.ownProperty('conversationId');
+				expect(contents.data[0]).to.have.ownProperty('state');
+				expect(contents.data[0]).to.have.ownProperty('privacy');
+				expect(contents.data[0]).to.have.ownProperty('title');
+				expect(contents.data[0]).to.have.ownProperty('content');
+				expect(contents.data[0]).to.have.ownProperty('summary');
+				expect(contents.data[0]).to.have.ownProperty('attachment');
+				expect(contents.data[0]).to.have.ownProperty('tags');
+				done();
+			})
+			.catch(function(err){
+				console.error(err);
+				done();
+			});
+	});
+it('should be get specific contents tips', function(done){
+		crawler.getTips()
+			.then(function(contents){
+				expect(contents).to.be.an('object');
+				expect(contents).to.have.ownProperty('totalRecords');
+				expect(contents.totalRecords).to.be.an('number');
+				expect(contents).to.have.ownProperty('totalDisplay');
+				expect(contents.totalDisplay).to.be.an('number');
+				expect(contents).to.have.ownProperty('data');
+				expect(contents).to.have.ownProperty('hasNext');
+				//data
+				if (contents.data[0]){
+					expect(contents.data[0]).to.have.ownProperty('type');
+					expect(contents.data[0]).to.have.ownProperty('conversationId');
+					expect(contents.data[0]).to.have.ownProperty('state');
+					expect(contents.data[0]).to.have.ownProperty('privacy');
+					expect(contents.data[0]).to.have.ownProperty('title');
+					expect(contents.data[0]).to.have.ownProperty('content');
+					expect(contents.data[0]).to.have.ownProperty('summary');
+					expect(contents.data[0]).to.have.ownProperty('attachment');
+					expect(contents.data[0]).to.have.ownProperty('tags');
+				}
+				done();
+			})
+			.catch(function(err){
+				console.error(err);
+				done();
+			});
+	});
+	it('should be get specific contents forums', function(done){
+		crawler.getForums()
+			.then(function(contents){
+				expect(contents).to.be.an('object');
+				expect(contents).to.have.ownProperty('totalRecords');
+				expect(contents.totalRecords).to.be.an('number');
+				expect(contents).to.have.ownProperty('totalDisplay');
+				expect(contents.totalDisplay).to.be.an('number');
+				expect(contents).to.have.ownProperty('data');
+				expect(contents).to.have.ownProperty('hasNext');
+				//data
+				if (contents.data[0]){
+					expect(contents.data[0]).to.have.ownProperty('type');
+					expect(contents.data[0]).to.have.ownProperty('conversationId');
+					expect(contents.data[0]).to.have.ownProperty('state');
+					expect(contents.data[0]).to.have.ownProperty('privacy');
+					expect(contents.data[0]).to.have.ownProperty('title');
+					expect(contents.data[0]).to.have.ownProperty('content');
+					expect(contents.data[0]).to.have.ownProperty('summary');
+					expect(contents.data[0]).to.have.ownProperty('attachment');
+					expect(contents.data[0]).to.have.ownProperty('tags');
+				}
+				done();
+			})
+			.catch(function(err){
+				console.error(err);
+				done();
+			});
+	});
+	it('should be get contents with params filter', function(done){
+		var param_filters = {
+			tags:"lele"
+		};
+		crawler.getContent(null,param_filters)
+			.then(function(contents){
+				expect(contents).to.be.an('object');
+				expect(contents).to.have.ownProperty('totalRecords');
+				expect(contents.totalRecords).to.be.an('number');
+				expect(contents).to.have.ownProperty('totalDisplay');
+				expect(contents.totalDisplay).to.be.an('number');
+				expect(contents).to.have.ownProperty('data');
+				expect(contents).to.have.ownProperty('hasNext');
+				//data
+				if (contents.data[0]){
+					expect(contents.data[0]).to.have.ownProperty('type');
+					expect(contents.data[0]).to.have.ownProperty('conversationId');
+					expect(contents.data[0]).to.have.ownProperty('state');
+					expect(contents.data[0]).to.have.ownProperty('privacy');
+					expect(contents.data[0]).to.have.ownProperty('privacy');
+					expect(contents.data[0]).to.have.ownProperty('title');
+					expect(contents.data[0]).to.have.ownProperty('content');
+					expect(contents.data[0]).to.have.ownProperty('summary');
+					expect(contents.data[0]).to.have.ownProperty('attachment');
+					expect(contents.data[0]).to.have.ownProperty('tags');
+					expect(contents.data[0].tags).to.be.an('array');
+					expect(contents.data[0].tags).to.include("Lele");
+				}
+				done();
+			})
+			.catch(function(err){
+				console.error(err);
+				done();
+			});
+	});
+	it('should be get contents with params sorting', function(done){
+		var param_sorts = {
+			dateSubmitted:"desc"
+		};
+		crawler.getContent(null,null,param_sorts)
+			.then(function(contents){
+				expect(contents).to.be.an('object');
+				expect(contents).to.have.ownProperty('totalRecords');
+				expect(contents.totalRecords).to.be.an('number');
+				expect(contents).to.have.ownProperty('totalDisplay');
+				expect(contents.totalDisplay).to.be.an('number');
+				expect(contents).to.have.ownProperty('data');
+				expect(contents).to.have.ownProperty('hasNext');
+				//data
+				if (contents.data[0]){
+					expect(contents.data[0]).to.have.ownProperty('type');
+					expect(contents.data[0]).to.have.ownProperty('conversationId');
+					expect(contents.data[0]).to.have.ownProperty('state');
+					expect(contents.data[0]).to.have.ownProperty('privacy');
+					expect(contents.data[0]).to.have.ownProperty('privacy');
+					expect(contents.data[0]).to.have.ownProperty('title');
+					expect(contents.data[0]).to.have.ownProperty('content');
+					expect(contents.data[0]).to.have.ownProperty('summary');
+					expect(contents.data[0]).to.have.ownProperty('attachment');
+					expect(contents.data[0]).to.have.ownProperty('tags');
+				}
+				done();
+			})
+			.catch(function(err){
+				console.error(err);
+				done();
+			});
+	});
 })
